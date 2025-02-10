96cb37c07f6594f2f247ab67cd78f0d50be16c66
SGhosh61 <Snehashis.Ghosh61@T-Mobile.com>
2025-02-09 21:58:25 -0800

    new component and design library

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Accordion from '../src/component/accordion';
 import ProgressBar from '../src/component/progressbar';
 import TabComponent from '../src/component/tabComponent';
+import Count from '../src/component/count';
 
 function App() {
   return (
     <div className="App">
-      <Accordion/>
+      {/* <Accordion/>
       <ProgressBar/>
-      <TabComponent/>
+      <TabComponent/> */}
+      <Count/>
     </div>
   );
 }
