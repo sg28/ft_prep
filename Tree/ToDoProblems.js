5fd7125f610c4d81f50e5287677dbaeed7167413
SGhosh61 <Snehashis.Ghosh61@T-Mobile.com>
2025-02-09 19:07:03 -0800

    tree problems

---
+++ Tree/ToDoProblems.js
@@ -0,0 +1,9 @@
+
+Tree
+1. Given a binary tree, determine if it is 
+height-balanced.
+2. Given a binary tree, find its minimum depth.
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+3. Binary Search Tree Iterator.Binary
+4. Count Complete Tree Nodes
+5. Lowest Common Ancestor of a Binary Search Tree.
