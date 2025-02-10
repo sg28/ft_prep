689bc9e60ae2bdffd1ac47207b5e98052d276cf9
SGhosh61 <Snehashis.Ghosh61@T-Mobile.com>
2025-02-09 23:07:10 -0800

    material UI design library

--- react_app/src/component/count.js
+++ react_app/src/component/count.js
@@ -1,23 +1,37 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
+import React from "react";
+import { useEffect, useState } from "react";
+import Button from '@mui/material/Button';
 
-function Count(){   
+function Count() {
+  let [counter, setcounter] = useState(0);
 
-    let [counter, setcounter] = useState(0);
-
-    return(
-        <>
-        <div>Count: {counter}</div>
-        <div>
-            <button
-                onClick={()=>{
-                    setcounter((prevCount)=>{
-                        return prevCount+1;
-                    })
-                }}
-            > Click Count</button>
-        </div>
-        </>
-    )
+  return (
+    <>
+      <div>Count: {counter}</div>
+      <div>
+        <Button variant="contained" color="success"
+          onClick={() => {
+            setcounter((prevCount) => {
+              return prevCount + 1;
+            });
+          }}
+          
+        >
+          [ + ]
+        </Button>
+        <Button variant="contained" color="danger"
+          onClick={() => {
+            setcounter((prevCount) => {
+              return prevCount - 1;
+            });
+          }}
+          
+        >
+          {" "}
+          [ - ]
+        </Button>
+      </div>
+    </>
+  );
 }
 export default Count;
