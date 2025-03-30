ba1720a43e669a87115559901999e992105d48a6
sg28 <g.snehasish@gmail.com>
2025-03-29 17:01:05 -0700

    traffic light component

--- react_app/src/component/trafficLight.js
+++ react_app/src/component/trafficLight.js
@@ -1,59 +1,95 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
-export default function TrafficLight(){
+export default function TrafficLight() {
+  const [light, setLight] = useState("red");
+  const [lights] = useState(["red", "orange", "green"]);
+  const [lightIndex, setLightIndex] = useState(0);
 
-    let[light,setLight] = useState("red");
-    
-    useEffect(()=>{
-        console.log("light ", light)
-        return (()=>{
-            console.log("grabage collect")
-            setLight("red")
-        })
-    },[])
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setLightIndex((prevIndex) => {
+        const nextIndex = (prevIndex + 1) % lights.length;
+        setLight(lights[nextIndex]);
+        return nextIndex;
+      });
+    }, 2000);
 
-    return(
-        <div className="main_component">
-            <div> Traffic Light</div>
-            <div className="lights" style={{
-                width: "25%",
-                margin: "auto",
-                padding: "5px",
-                marginTop:"5px",
-                borderRadius:"5px",
-                border:"1px solid lightblue"
-            }}>
-                <div className="red" 
-                style={
-                    {
-                        backgroundColor:"red",
-                        border:"1px solid red",
-                        borderRadius:"4px",
-                        margin:"2px"
-                    }
-                }> 
-                red </div>
-                <div className="orange"
-                style={
-                    {
-                        backgroundColor:"orange",
-                        border:"1px solid orange",
-                        borderRadius:"4px",
-                        margin:"2px"
-                    }
-                }
-                > oraange </div>
-                <div className="green"
-                    style={
-                        {
-                            backgroundColor:"green",
-                            border:"1px solid green",
-                            borderRadius:"4px",
-                            margin:"2px"
-                        }
-                    }
-                > green </div>
+    return () => {
+      clearInterval(interval);
+      setLight("red");
+    };
+  }, [lights]);
+
+  return (
+    <>
+      <style>{`
+        .main-component {
+          text-align: center;
+        }
+
+        .title {
+          font-weight: bold;
+          margin-bottom: 10px;
+        }
+
+        .lights-container {
+          width: 25%;
+          margin: auto;
+          padding: 5px;
+          margin-top: 5px;
+          border-radius: 5px;
+          border: 1px solid lightblue;
+        }
+
+        .light-box {
+          border-radius: 4px;
+          margin: 2px;
+          border: 1px solid transparent;
+          color: #fff;
+          padding: 10px;
+          text-transform: capitalize;
+        }
+
+        .light-box.red {
+          border-color: red;
+        }
+
+        .light-box.orange {
+          border-color: orange;
+        }
+
+        .light-box.green {
+          border-color: green;
+        }
+
+        .light-box.active.red {
+          background-color: red;
+        }
+
+        .light-box.active.orange {
+          background-color: orange;
+        }
+
+        .light-box.active.green {
+          background-color: green;
+        }
+      `}</style>
+
+      <div className="main-component">
+        <div className="title">Traffic Light</div>
+        <div className="lights-container">
+          {lights.map((lightColor, index) => (
+            <div
+              key={index}
+              className={`light-box ${lightColor} ${
+                index === lightIndex ? "active" : ""
+              }`}
+            >
+              {lightColor}
             </div>
+          ))}
         </div>
-    )
+      </div>
+    </>
+  );
 }
