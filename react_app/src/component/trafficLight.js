a2f58d6f12939ddfbb7b8efa4250eb8539cd86a9
sg28 <g.snehasish@gmail.com>
2025-03-29 17:20:11 -0700

    update index selection logic in traffic light

--- react_app/src/component/trafficLight.js
+++ react_app/src/component/trafficLight.js
@@ -1,95 +1,56 @@
 import React, { useState, useEffect } from "react";
 
 export default function TrafficLight() {
-  const [light, setLight] = useState("red");
-  const [lights] = useState(["red", "orange", "green"]);
-  const [lightIndex, setLightIndex] = useState(0);
+    const [lights] = useState(["red", "orange", "green"]);
+    const [lightIndex, setLightIndex] = useState(0);
+  
+    useEffect(() => {
+        const interval = setInterval(() => {
+          setLightIndex((prevIndex) => {
+            // const nextIndex = (prevIndex + 1) % lights.length;
+            const nextIndex = prevIndex + 1 === lights.length ? 0 : prevIndex + 1;
+            return nextIndex;
+          });
+        }, 2000);
+    
+        return () => {
+          clearInterval(interval);
+        };
+      }, [lights]);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setLightIndex((prevIndex) => {
-        const nextIndex = (prevIndex + 1) % lights.length;
-        setLight(lights[nextIndex]);
-        return nextIndex;
-      });
-    }, 2000);
-
-    return () => {
-      clearInterval(interval);
-      setLight("red");
-    };
-  }, [lights]);
 
   return (
-    <>
-      <style>{`
-        .main-component {
-          text-align: center;
-        }
-
-        .title {
-          font-weight: bold;
-          margin-bottom: 10px;
-        }
-
-        .lights-container {
-          width: 25%;
-          margin: auto;
-          padding: 5px;
-          margin-top: 5px;
-          border-radius: 5px;
-          border: 1px solid lightblue;
-        }
-
-        .light-box {
-          border-radius: 4px;
-          margin: 2px;
-          border: 1px solid transparent;
-          color: #fff;
-          padding: 10px;
-          text-transform: capitalize;
-        }
-
-        .light-box.red {
-          border-color: red;
-        }
-
-        .light-box.orange {
-          border-color: orange;
-        }
-
-        .light-box.green {
-          border-color: green;
-        }
-
-        .light-box.active.red {
-          background-color: red;
-        }
-
-        .light-box.active.orange {
-          background-color: orange;
-        }
-
-        .light-box.active.green {
-          background-color: green;
-        }
-      `}</style>
-
-      <div className="main-component">
-        <div className="title">Traffic Light</div>
-        <div className="lights-container">
-          {lights.map((lightColor, index) => (
+    <div className="main_component">
+      <div> Traffic Light</div>
+      <div
+        className="lights"
+        style={{
+          width: "25%",
+          margin: "auto",
+          padding: "5px",
+          marginTop: "5px",
+          borderRadius: "5px",
+          border: "1px solid lightblue",
+        }}
+      >
+        {lights.map((light, index) => {
+          return (
             <div
               key={index}
-              className={`light-box ${lightColor} ${
-                index === lightIndex ? "active" : ""
-              }`}
+              className={light}
+              style={{
+                backgroundColor: index === lightIndex ? light : '#fff',
+                border: `1px solid ${light}`,
+                borderRadius: "4px",
+                margin: "2px",
+                color:"#fff"
+              }}
             >
-              {lightColor}
+              {light}{" "}
             </div>
-          ))}
-        </div>
+          );
+        })}
       </div>
-    </>
+    </div>
   );
 }
