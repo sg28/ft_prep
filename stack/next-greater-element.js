944e0d43bbe4d2090281eb7cea72d32c3b0cc47f
sg28 <svrddhig@gmail.com>
2025-05-27 00:05:09 -0700

    monotonic

--- stack/next-greater-element.js
+++ stack/next-greater-element.js
@@ -16,3 +16,26 @@ Explanation: The first 1's next greater number is 2;
 The number 2 can't find next greater number. 
 The second 1's next greater number needs to search circularly, which is also 2.
 */
+
+function nextGreaterElements(nums) {
+    const n = nums.length;
+    const res = new Array(n).fill(-1);
+    const stack = [];
+
+    // Traverse the array twice to simulate circularity
+    for (let i = 0; i < 2 * n; i++) {
+        let num = nums[i % n];
+        while (stack.length && nums[stack[stack.length - 1]] < num) {
+            let idx = stack.pop();
+            res[idx] = num;
+        }
+        if (i < n) stack.push(i);
+    }
+    return res;
+}
+
+// Example usage:
+console.log(nextGreaterElements([1,2,1])); // [2, -1, 2]
+console.log(nextGreaterElements([1,2,3,4,3])); // [2,3,4,-1,4]
+
+// monotonic decreasing stack
