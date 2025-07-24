6e424ca4c115ed544ac29bc4404b387eafdbf2a6
sg28 <svrddhig@gmail.com>
2025-07-24 02:47:12 -0700

    sliding window

--- sliding-window/LongestStringWithDistinctChar.js
+++ sliding-window/LongestStringWithDistinctChar.js
@@ -10,27 +10,26 @@
 */
 
 function lengthOfLongestSubstringTwoDistinct(s) {
-    let left = 0;
-    let maxLen = 0;
-    let map = {};
+  let left = 0;
+  let maxLen = 0;
+  let map = {};
 
-    for (let right = 0; right < s.length; right++) {
-        let char = s[right];
-        map[char] = (map[char] || 0) + 1;
+  for (let right = 0; right < s.length; right++) {
+    let char = s[right];
+    map[char] = (map[char] || 0) + 1;
 
-        // Shrink window if more than 2 distinct chars
-        while (Object.keys(map).length > 2) {
-            let leftChar = s[left];
-            map[leftChar]--;
-            if (map[leftChar] === 0) {
-                delete map[leftChar];
-            }
-            left++;
-        }
-
-        maxLen = Math.max(maxLen, right - left + 1);
+    while (Object.keys(map).length > 2) {
+      let leftChar = s[left];
+      map[leftChar]--;
+      if (map[leftChar] === 0) {
+        delete map[leftChar];
+      }
+      left++;
     }
-    return maxLen;
+
+    maxLen = Math.max(maxLen, right - left + 1);
+  }
+  return maxLen;
 }
 
 console.log(lengthOfLongestSubstringTwoDistinct("eceba")); // Output: 3
