be45aa859b7f51ff700c4c12c0dd40f9faf3a2f0
sg28 <svrddhig@gmail.com>
2025-07-30 03:38:14 -0700

    update category selection

--- react_app/src/container/shopingcart/index.js
+++ react_app/src/container/shopingcart/index.js
@@ -2,19 +2,27 @@ import React,{useEffect} from 'react';
 import { useState } from 'react';
 import Header from "../../component/header";
 import Category from '../../component/category';
+
 import Men from '../../component/ItemContainer/Men';
 import Women from '../../component/ItemContainer/Women';
 
+
+ 
 export default function ShopingCart(props={}){
 
-    let [categorySelected, setCategorySelected] = useState("Men");
+    let [categorySelected, setCategorySelected] = useState({key: 1, name: 'Men'});
     
     function loadItemComponent() {
         if (categorySelected.name === 'Men') {
-            console.log('Men selected')
             return <Men />;
         } else if (categorySelected.name === 'Women') {
             return <Women />;
+        } else if (categorySelected.name === 'Children') {
+            return <div>Children's products coming soon.</div>;
+        } else if (categorySelected.name === 'Bags') {
+            return <div>Bags products coming soon.</div>;
+        } else if (categorySelected.name === 'Shoes') {
+            return <div>Shoes products coming soon.</div>;
         } else {
             return null;
         }
@@ -28,7 +36,7 @@ export default function ShopingCart(props={}){
 
     // onload of shoping cart
     useEffect(()=>{
-        console.log("onload shoping cart")
+        console.log("onload shoping cart, categorySelected ", categorySelected)
     },[])
 
 
