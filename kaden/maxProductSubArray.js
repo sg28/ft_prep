338bbdf6089d3a970836091d13fe6b6d9b80008a
Snehashis Ghosh <Snehashis.Ghosh61@T-Mobile.com>
2025-09-10 22:07:08 -0700

    kaden algorithm

--- kaden/maxProductSubArray.js
+++ kaden/maxProductSubArray.js
@@ -1,3 +1,11 @@
+
+/*
+    Given an integer array nums, find the contiguous subarray (containing at least one number) 
+    which has the largest sum and return its sum.
+
+*/
+
+
 export default function maxProductSubArray(numbers) {
   let currentMax = numbers[0];
   let currentMin = numbers[0];
--- kaden/maxSumSubArray.js
+++ kaden/maxSumSubArray.js
@@ -1,3 +1,23 @@
+/*
+
+Given an integer array nums, find the contiguous subarray (containing at least one number) 
+which has the largest sum and return its sum.
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+
+Input: nums = [1]
+Output: 1
+
+Input: nums = [5,4,-1,7,8]
+Output: 23
+Explanation: [5,4,-1,7,8] has the largest sum = 23.
+
+*/
+
+
+
 export default function maxSumSubArray(numbers) {
   let current = numbers[0];
   let best = numbers[0];
