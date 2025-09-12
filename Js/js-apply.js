47f57b354cdcdf5b5b83484ae04ce5950f71001d
Snehashis Ghosh <Snehashis.Ghosh61@T-Mobile.com>
2025-09-12 03:39:59 -0700

    js apply code

---
+++ Js/js-apply.js
@@ -0,0 +1,21 @@
+
+function human(...args) {
+    let list = [...args];
+    return this.name + "has a"+list[0]+" and a "+list[1];
+}
+
+let Indian = {
+    name: "Rakesh",
+    age: 30
+};
+let American = {
+    name: "James",
+    age: 44
+};
+
+let entity = human.apply(Indian, ["black-hair", "brown-eye"]);
+console.log("entity ", entity);
+
+
+
+
