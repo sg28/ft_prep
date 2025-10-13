69d6021809bf280723bc2a0c9233362a9b0e7d6a
Snehashis Ghosh <Snehashis.Ghosh61@T-Mobile.com>
2025-10-12 20:17:20 -0700

    update

--- interviews/frontend_technical_assessment/start_frontend.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Script to start the frontend server
-
-cd /Users/sghosh61/Documents/sg/ft_prep/interviews/frontend_technical_assessment/frontend
-echo "Starting React development server..."
-npm start
