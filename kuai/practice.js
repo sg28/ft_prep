274be1398fb7e6deca5852c9591760f82336b9c4
sg28 <svrddhig@gmail.com>
2026-07-31 12:09:11 -0700

    kuai and ust resume update

---
+++ kuai/practice.js
@@ -0,0 +1,12 @@
+
+/*  
+
+### [Count Number of Teams]
+(https://leetcode.com/problems/count-number-of-teams/)
+**Problem Statement**: You are given an array of soldier ratings. 
+A team of 3 soldiers is considered increasing if their ratings are
+ordered such that `rating[i] < rating[j] < rating[k]` and `i < j < k`. 
+Similarly, a team is considered decreasing if `rating[i] > rating[j] > rating[k]` and `i < j < k`. 
+A team can be formed if it's either increasing or decreasing. Your task is to return the number of teams you can form given the ratings.
+*/
+
