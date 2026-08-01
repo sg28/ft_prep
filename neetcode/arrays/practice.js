e1bfd5970440823ac4d5c4c10efecc2f835c9db0
Snehashis Ghosh <svrddhig25@gmail.com>
2026-08-01 15:38:55 -0700

    refactor

--- neetcode/arrays/practice.js
+++ neetcode/arrays/practice.js
@@ -1,28 +1,10 @@
 
-//  kaden's algorithm
-
-function kaden(
-    nums
-){
-    let maxSum = nums[0];
+// kaden
+function kaden(nums){
+    let sum = 0;
     let curSum = 0;
-    let start, end = 0;
     let left = 0;
+    let start = 0, end = 0;
 
-    for(let right = 0; right < nums.length; right++){
-
-        if(curSum < 0){
-            curSum = 0;
-            left = right;
-        }
-
-        curSum += nums[right];
-        if(curSum > maxSum){
-            maxSum = curSum;
-            start = left;
-            end = right;
-        }
-    }
-    return {maxSum, subArray:[start,end]};
-
+    
 }
